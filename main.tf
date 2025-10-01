9ba937ebb234ed2cee72f10aa76c9914f269cbb2
moon84pl <71186172+moon84pl@users.noreply.github.com>
2025-10-01 09:39:54 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-02"
+    storage_account_name = "warsztatystorage33123123"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example-app-service-plan-daosdjoias12"
+  location            = "westeurope" 
+  resource_group_name = "rg-02"
+  os_type             = "Linux"
+  sku_name            = "P0v3"
+}
+
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "example-webapp-asdoi3221op3kadasd"
+  location            = "westeurope"
+  resource_group_name = "rg-02"
+  service_plan_id     = azurerm_service_plan.example.id
+  site_config {}
+}
